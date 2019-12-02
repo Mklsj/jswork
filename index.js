3d5e500e4e7b1b54c4e987645fa8866eee4eae45
Ainili2 <2326529751@qq.com>
2019-12-02 18:57:31 +0800

    work13

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-var curIndex = 0;
-//时间间隔(单位毫秒)，每秒钟显示一张，数组共有3张图片放在img文件夹下
-var timeInterval = 1000;
+setInterval(function () {
+    $.get('http://api.youngam.cn/picapi.php', function (data) {
+        $('#pic_content').prepend(
+            '<li><img src="' + data + '" alt=""></li>');
+    });
 
-                        //定义一个存放照片位置的数组，可以放任意个，在这里放3个
-var arr = new Array();
-arr[0] = "img/1.jpg";
-arr[1] = "img/2.jpg";
-arr[2] = "img/3.jpg";
-arr[3] = "img/4.jpg";
-arr[4] = "img/5.jpg";
-arr[5] = "img/10.jpg";
-setInterval(changeImg, timeInterval);
-function changeImg() {
-if (curIndex == arr.length - 1) {
-curIndex = 0;
-} else {
-curIndex += 1;
-}
-
-                                  //设置body的背景图片
-document.body.style.backgroundImage = "URL("+arr[curIndex]+")";  //显示对应的图片
-}
+//删除最后一个li
+$('#pic_content li:last-child').remove();
+$('#pic_content li:last-child').css({
+    transform: 'scale(1.1) rotate(' + parseInt((Math.random() - 0.5) * 20) + 'deg)',
+    opacity: '0'
+});
+}, 8000);
